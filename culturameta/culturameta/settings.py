de285d825fe278a8c6568e8a0b6525bb868dd21b
jorge <jorgecuenca.unity@gmail.com>
2021-08-17 21:35:16 -0500

    sdsd

--- culturameta/culturameta/settings.py
+++ culturameta/culturameta/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static.jorgecuenca.info/culturameta/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'static'),
 )
@@ -143,4 +143,4 @@ STATICFILES_FINDERS=[
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
-MEDIA_URL='static.jorgecuenca.info/culturameta/media/'
+MEDIA_URL='/media/'
