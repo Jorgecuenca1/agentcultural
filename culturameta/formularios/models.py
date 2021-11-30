3f4dabeef64a2cf5d6697dd694f158484f8f1285
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2021-11-30 15:45:16 -0500

    kk

--- culturameta/formularios/models.py
+++ culturameta/formularios/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 
-
+from django.contrib.auth.models import User
 from django_file_validator.validators import MaxSizeValidator
 from django.core.exceptions import ValidationError
 from multiselectfield import MultiSelectField
@@ -342,3 +342,27 @@ class Filarmonica(models.Model):
     class Meta:
         verbose_name = 'FILARMONICA'
         verbose_name_plural = 'FILARMONICA'
+
+class Contratista(models.Model):
+    user = models.OneToOneField(User, verbose_name='Creador de actividad', on_delete=models.PROTECT, blank=True,
+                                null=True)
+    name = models.CharField(max_length=50, blank=True, verbose_name='Nombres', null=True)
+    last_name = models.CharField(max_length=50, blank=True, verbose_name='Apeliidos', null=True)
+    area = models.CharField(max_length=50, blank=True, verbose_name='Area de trabajo(ejemplo musica)', null=True)
+    codigo = models.CharField(max_length=50, blank=True, verbose_name='Codigo contrato', null=True)
+    objeto = models.TextField(blank=True, verbose_name='Objeto del contrato', null=True)
+    email = models.EmailField(max_length=20, blank=True, verbose_name='Correo *', null=True)
+    phone = models.IntegerField(max_length=100, blank=True, verbose_name='Teléfono fijo:', null=True)
+    titulo = models.CharField(max_length=50, blank=True, verbose_name='Titulo del trabajo', null=True)
+    justifique = models.TextField(blank=True, verbose_name='Justifique su trabajo', null=True)
+    document = models.FileField(verbose_name='Documento', upload_to='document/contratista', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'FILARMONICA'
+        verbose_name_plural = 'FILARMONICA'
+
